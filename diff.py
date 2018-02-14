26036cb383a71fec9e9b12f1cd8f659f6d6f6a6e
xdass <420600@gmail.com>
2018-02-14 15:06:56 +0500

    Dev-process:sync with github for distributed work

--- diff.py
+++ diff.py
@@ -7,6 +7,7 @@ __copyright__ = '(C) 2003 Aaron Swartz. GNU GPL 2 or 3.'
 __version__ = '0.22'
 
 import difflib, string
+import re
 
 
 def isTag(x): return x[0] == "<" and x[-1] == ">"
@@ -15,8 +16,9 @@ def isTag(x): return x[0] == "<" and x[-1] == ">"
 def textDiff(a, b):
     """Takes in strings a and b and returns a human-readable HTML diff."""
     out = []
-    test_out = []
-    a, b = html2list(a), html2list(b)
+    #a, b = html2list(a), html2list(b)
+    a = a.splitlines()
+    b = b.splitlines()
     try:  # autojunk can cause malformed HTML, but also speeds up processing.
         s = difflib.SequenceMatcher(None, a, b, autojunk=True)
     except TypeError:
@@ -24,33 +26,32 @@ def textDiff(a, b):
     print(a)
     print(b)
     for e in s.get_opcodes():
-        print(out)
+        print(e)
         if e[0] == "replace":
             out.append('<span class="deleted">'+''.join(a[e[1]:e[2]]) + '</span><span class="added">'+''.join(b[e[3]:e[4]])+"</span>")
         elif e[0] == "delete":
-            # print(e)
-            # print(a[e[1]:e[2]], 'deleted from ', e[1], e[2])
             out.append('<span class="deleted">' + ''.join(a[e[1]:e[2]]) + '</span>')
         elif e[0] == "insert":
-            print(e)
             out.append('<span class="added">'+''.join(b[e[3]:e[4]]) + "</span>")
         elif e[0] == "equal":
             if e[1] != e[3] and e[2] != e[4]:  # Moved
-                print(e)
-                #print(a[e[3]:e[4]])
-                print(a[e[1]:e[2]-1], ' delete from ', e[1], e[2])
-                print('Moved to ', e[3], e[4])
-                out.append('<span class="moved">' + ''.join(b[e[3]:e[4]-1]) + '</span>')
+                print(b[e[3]:e[4]])
+                print(a[e[1]:e[2]])
+                out.append('<span class="moved">' + ''.join(b[e[3]:e[4]]) + '</span>')
             else:
-                out.append(''.join(b[e[3] - 1:e[4]]))
-                if e[3] > 0:
-                    out.append(''.join(b[e[3] - 1:e[4]]))
-                else:
-                    out.append(''.join(b[e[3]:e[4]]))
+                out.append(''.join(b[e[3]:e[4]]))
         else:
             raise "Um, something's broken. I didn't expect a '" + e[0] + "'."
     print(out)
     return ''.join(out)
+
+
+def is_tag(item):
+    result = re.match(r'<.*>(.*)</.*?>', item[0])
+    if result:
+        return False
+    else:
+        return True
 
 
 def html2list(x, b=0):
